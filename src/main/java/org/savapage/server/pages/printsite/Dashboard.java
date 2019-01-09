01cb3034e1ecc4732f6f452cd9fb6bf877852130
Rijk Ravestein <rijkr@savapage.org>
2019-01-09 19:53:27 +0100

    Add Print Site Web App (in progress)
    See: #980

--- src/main/java/org/savapage/server/pages/printsite/Dashboard.java
+++ src/main/java/org/savapage/server/pages/printsite/Dashboard.java
@@ -22,8 +22,15 @@
 package org.savapage.server.pages.printsite;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.savapage.core.dao.PrintOutDao;
 import org.savapage.core.dao.enums.ACLOidEnum;
+import org.savapage.core.dao.enums.ExternalSupplierEnum;
+import org.savapage.core.dao.enums.ExternalSupplierStatusEnum;
+import org.savapage.core.i18n.NounEnum;
 import org.savapage.core.i18n.PhraseEnum;
+import org.savapage.core.i18n.PrintOutNounEnum;
+import org.savapage.core.services.ServiceContext;
+import org.savapage.ext.papercut.services.PaperCutService;
 import org.savapage.server.helpers.HtmlButtonEnum;
 import org.savapage.server.pages.MarkupHelper;
 
@@ -38,6 +45,22 @@ public class Dashboard extends AbstractPrintSitePage {
      * Version for serialization.
      */
     private static final long serialVersionUID = 1L;
+
+    /** */
+    private static final String WID_PROMPT_PENDING_EXT_USERS =
+            "prompt-pending-ext-users";
+
+    /** */
+    private static final String WID_PROMPT_PENDING_CUPS_USERS =
+            "prompt-pending-cups-users";
+
+    /** */
+    private static final PrintOutDao PRINT_OUT_DAO =
+            ServiceContext.getDaoContext().getPrintOutDao();
+
+    /** */
+    private static final PaperCutService PAPERCUT_SERVICE =
+            ServiceContext.getServiceFactory().getPaperCutService();
 
     /**
      * @param parameters
@@ -55,6 +78,73 @@ public class Dashboard extends AbstractPrintSitePage {
         helper.addLabel("realtime-activity",
                 PhraseEnum.REALTIME_ACTIVITY.uiText(getLocale()));
 
+        if (PAPERCUT_SERVICE.checkPrintIntegration()) {
+
+            helper.discloseLabel(WID_PROMPT_PENDING_CUPS_USERS);
+
+            helper.addLabel(WID_PROMPT_PENDING_EXT_USERS,
+                    NounEnum.USER.uiText(getLocale(), true));
+
+            helper.addLabel("prompt-pending-ext-jobs",
+                    PrintOutNounEnum.JOB.uiText(getLocale(), true));
+
+            final long nJobs = PRINT_OUT_DAO.countExtSupplierJobs(
+                    ExternalSupplierEnum.SAVAPAGE,
+                    ExternalSupplierStatusEnum.PENDING_EXT);
+
+            final String countUsers;
+            final String countJobs;
+
+            if (nJobs == 0) {
+                countJobs = "-";
+                countUsers = "-";
+            } else {
+                countJobs = helper.localizedNumber(nJobs);
+                if (nJobs == 1) {
+                    countUsers = countJobs;
+                } else {
+                    countUsers = helper.localizedNumber(
+                            PRINT_OUT_DAO.countExtSupplierJobUsers(
+                                    ExternalSupplierEnum.SAVAPAGE,
+                                    ExternalSupplierStatusEnum.PENDING_EXT));
+                }
+            }
+
+            helper.addLabel("pending-ext-users", countUsers);
+            helper.addLabel("pending-ext-jobs", countJobs);
+
+        } else {
+
+            helper.discloseLabel(WID_PROMPT_PENDING_EXT_USERS);
+
+            // CUPS
+            helper.addLabel(WID_PROMPT_PENDING_CUPS_USERS,
+                    NounEnum.USER.uiText(getLocale(), true));
+
+            helper.addLabel("prompt-pending-cups-jobs",
+                    PrintOutNounEnum.JOB.uiText(getLocale(), true));
+
+            final long nJobs = PRINT_OUT_DAO.countActiveCupsJobs();
+
+            final String countUsers;
+            final String countJobs;
+
+            if (nJobs == 0) {
+                countJobs = "-";
+                countUsers = "-";
+            } else {
+                countJobs = helper.localizedNumber(nJobs);
+                if (nJobs == 1) {
+                    countUsers = countJobs;
+                } else {
+                    countUsers = helper.localizedNumber(
+                            PRINT_OUT_DAO.countActiveCupsJobUsers());
+                }
+            }
+
+            helper.addLabel("pending-cups-users", countUsers);
+            helper.addLabel("pending-cups-jobs", countJobs);
+        }
     }
 
 }
