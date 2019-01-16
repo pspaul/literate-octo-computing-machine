f0fc80c89ffd5730a5792f0247c4114ed6b9ae47
Rijk Ravestein <rijkr@savapage.org>
2019-01-16 16:16:28 +0100

    Add user group check in Smartschool notification plug-in
    See: #1025

--- src/main/java/org/savapage/server/ext/ServerPluginManager.java
+++ src/main/java/org/savapage/server/ext/ServerPluginManager.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the SavaPage project <https://www.savapage.org>.
- * Copyright (c) 2011-2018 Datraverse B.V.
+ * Copyright (c) 2011-2019 Datraverse B.V.
  * Author: Rijk Ravestein.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -1395,6 +1395,12 @@ public final class ServerPluginManager implements PaymentGatewayListener,
     @Override
     public File getPluginHome() {
         return ConfigManager.getServerExtHome();
+    }
+
+    @Override
+    public boolean isUserInGroup(final String groupName, final String userId) {
+        return ServiceContext.getDaoContext().getUserGroupMemberDao()
+                .isUserInGroup(groupName, userId);
     }
 
 }
