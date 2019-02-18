4b9568cc06ebb6a7397139c5b8d765356737eb52
Rijk Ravestein <rijkr@savapage.org>
2019-02-18 19:22:39 +0100

    Fix logger class

--- src/main/java/org/savapage/server/webapp/AbstractWebAppPage.java
+++ src/main/java/org/savapage/server/webapp/AbstractWebAppPage.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the SavaPage project <https://www.savapage.org>.
- * Copyright (c) 2011-2018 Datraverse B.V.
+ * Copyright (c) 2011-2019 Datraverse B.V.
  * Author: Rijk Ravestein.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -67,7 +67,7 @@ public abstract class AbstractWebAppPage extends AbstractPage
 
     /** */
     private static final Logger LOGGER =
-            LoggerFactory.getLogger(AbstractPage.class);
+            LoggerFactory.getLogger(AbstractWebAppPage.class);
 
     /**
      * .
