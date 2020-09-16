1f512072d9f8266e136c590f12b1ddcc16c1b028
Rijk Ravestein <rijkr@savapage.org>
2020-09-16 13:26:40 +0200

    Correct minor syntax issue

--- src/main/webapp/jquery.savapage.js
+++ src/main/webapp/jquery.savapage.js
@@ -2559,11 +2559,13 @@
              * Create temp <div> to determine overflow scrollbar width.
              * */
             this.getOverflowScrollBarWidth = function() {
-                var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div></div>');
+                var w1,
+                    w2,
+                    div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div></div>');
                 $('body').append(div);
-                var w1 = $('div', div).innerWidth();
+                w1 = $('div', div).innerWidth();
                 div.css('overflow-y', 'auto');
-                var w2 = $('div', div).innerWidth();
+                w2 = $('div', div).innerWidth();
                 $(div).remove();
                 return (w1 - w2);
             };
